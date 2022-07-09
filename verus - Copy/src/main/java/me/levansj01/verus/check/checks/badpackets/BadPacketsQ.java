6c34229e992ed58a0f8d08be76dbcb4e20b0f447
FluteVegetables <84094218+FluteVegetables@users.noreply.github.com>
2022-07-09 08:33:28 -0400

    Add files via upload

---
+++ verus - Copy/src/main/java/me/levansj01/verus/check/checks/badpackets/BadPacketsQ.java
@@ -0,0 +1,36 @@
+package main.java.me.levansj01.verus.check.checks.badpackets;
+
+import me.levansj01.verus.check.PacketCheck;
+import me.levansj01.verus.check.type.CheckType;
+import me.levansj01.verus.check.version.CheckVersion;
+import me.levansj01.verus.compat.VPacket;
+import me.levansj01.verus.compat.packets.VPacketPlayInFlying;
+import me.levansj01.verus.compat.packets.VPacketPlayInUseEntity;
+import me.levansj01.verus.data.version.ClientVersion;
+
+@CheckInfo(
+        type = CheckType.BAD_PACKETS, 
+        subType = "Q", 
+        friendlyName = "KillAura", 
+        version = CheckVersion.EXPERIMENTAL
+)
+
+public class BadPacketsQOld extends PacketCheck {
+
+    private boolean sent = false;
+
+    public void handle(VPacket vPacket, long l) {
+        if (vPacket instanceof VPacketPlayInFlying) {
+            this.sent = false;
+        } else if (vPacket instanceof VPacketPlayInUseEntity) {
+            VPacketPlayInUseEntity vPacketPlayInUseEntity = (VPacketPlayInUseEntity) vPacket;
+            if (vPacketPlayInUseEntity.getAction().isInteract()) {
+                if (this.playerData.getVersion() == ClientVersion.VERSION1_8 && !this.sent) {
+                    this.handleViolation("", 1.0, true);
+                }
+            } else if (vPacketPlayInUseEntity.getAction().isInteractAt()) {
+                this.sent = true;
+            }
+        }
+    }
+}
