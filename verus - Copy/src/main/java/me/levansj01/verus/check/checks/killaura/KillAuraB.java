c7045ab9d8b08145df0f4998cff4298560af8266
FluteVegetables <84094218+FluteVegetables@users.noreply.github.com>
2022-07-25 20:46:32 -0400

    Add normal aura checks (unfinished)

---
+++ verus - Copy/src/main/java/me/levansj01/verus/check/checks/killaura/KillAuraB.java
@@ -0,0 +1,22 @@
+package me.levansj01.verus.check.checks.killaura;
+
+import me.levansj01.verus.check.PacketCheck;
+import me.levansj01.verus.check.annotation.CheckInfo;
+import me.levansj01.verus.check.type.CheckType;
+import me.levansj01.verus.check.version.CheckVersion;
+import me.levansj01.verus.compat.VPacket;
+import me.levansj01.verus.compat.packets.VPacketPlayInFlying;
+import me.levansj01.verus.compat.packets.VPacketPlayInUseEntity;
+
+@CheckInfo(
+  type = CheckType.KILL_AURA, 
+  subType = "B", 
+  friendlyName = "KillAura", 
+  version = CheckVersion.RELEASE, 
+  minViolations = -1.0, 
+  maxViolations = 4, 
+  logData = true
+)
+public class KillAuraB extends PacketCheck {
+  
+}
